20bd9234b21b0dcb2e5457b33b6079ba737d9856
Ridge <ridge.telegrafi@icloud.com>
2025-05-10 03:32:18 -0400

    created and manipulate JavaScript objects to handle attendees

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 //Write your code here
 
+let attendee = { attendeeId: "T001",
+  name:"Alice Smith",
+  event:"JavaScript Conference",
+  ticketType:"VIP",
+  ticketPrice:150.00
+}
 
+function logAttendeeName(attendee) { console.log(attendee.name);
+}
+
+function logTicketPrice(attendee) {console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee,newTicketType) {attendee["ticketType"]= newTicketType;
+}
+
+function updateTicketPrice(attendee,newTicketPrice) {attendee["ticketPrice"]=newTicketPrice;
+}
+
+function removeEventProperty (attendee) { delete attendee.event; 
+}
+
+function addCheckedInProperty (attendee) {attendee.checkedIn =true; 
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
